1ab0fd3dc53116283e6e2ef9abc559d2bca675f5
iamrith <chantharith77@gmail.com>
2025-03-06 09:45:33 +0700

    Delete the calling the function tor chatting with chatbot in terminal

--- ai-model/src/inference.py
+++ ai-model/src/inference.py
@@ -2,10 +2,10 @@ import joblib
 from preprocess import preprocess
 
 # Load trained model and vectorizer
-vectorizer = joblib.load("ai-model/model/vectorizer.pkl")
-model = joblib.load("ai-model/model/model.pkl")
-label_map = joblib.load("ai-model/model/label_map.pkl")
-responses = joblib.load("ai-model/model/responses.pkl")
+vectorizer = joblib.load("/Users/nychanthrith/Documents/Chantharith/ME-Chatbot/ai-model/model/vectorizer.pkl")
+model = joblib.load("/Users/nychanthrith/Documents/Chantharith/ME-Chatbot/ai-model/model/model.pkl")
+label_map = joblib.load("/Users/nychanthrith/Documents/Chantharith/ME-Chatbot/ai-model/model/label_map.pkl")
+responses = joblib.load("/Users/nychanthrith/Documents/Chantharith/ME-Chatbot/ai-model/model/responses.pkl")
 
 def get_response(user_input):
     processed_input = preprocess(user_input)
@@ -14,11 +14,11 @@ def get_response(user_input):
     return responses[predicted_label][0]  # Return the best response
 
 # Simple chatbot interaction
-print("Chatbot is ready! Type 'exit' to quit.")
+# print("Chatbot is ready! Type 'exit' to quit.")
 
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == "exit":
-        break
-    response = get_response(user_input)
-    print(f"ME Chatbot: {response}")
+# while True:
+#     user_input = input("You: ")
+#     if user_input.lower() == "exit":
+#         break
+#     response = get_response(user_input)
+#     print(f"ME Chatbot: {response}")
